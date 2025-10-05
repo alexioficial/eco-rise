1b487114f9dca7210d7a71c420cfd573dc0dcf6f
alexioficial <grullonmatiasalexi@gmail.com>
2025-10-04 21:28:10 -0400

    ya woa empezar a poner a gemini

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 from utils.ai import tools
 
+tools.save_page_screenshot("https://cat.csiss.gmu.edu/CropSmart", "static/imgs/data/img1.png", full_page=True)
 tools.save_page_screenshot("https://cat.csiss.gmu.edu/CropSmart#map=7.76/-106.85/43.00", "static/imgs/data/img2.png", full_page=True)
